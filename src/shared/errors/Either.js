657b06836fc9420576a0be36ace8752dfa8a5511
Gabriel Chagas <gabrielserq@outlook.com>
2025-02-02 22:51:58 -0300

    feat: add either.js

---
+++ src/shared/errors/Either.js
@@ -0,0 +1,31 @@
+/**
+ * @description: Atenção, está classe não deve ser instânciada diretamente, use um dos métodos Left ou Right
+ */
+
+module.exports= class Either{
+  constructor(left, right){
+    this.left = left
+    this.right = right
+  }
+
+  static Left(left){
+    return new Either(left, null)
+  }
+
+  static Right(right){
+    return new Either(null, right)
+  }
+
+  static valorJaCadastrado(valor){
+    return { message: `${valor} já cadastrado.` }
+  }
+
+  static dataRetornoMenorQueDataSaida = {
+    message: 'Data de retorno menor que a data de saída'
+  }
+
+  static livroComISBNJaEmprestadoPendenteUsuario = {
+    message: 'Livro com ISBN já emprestado ao usuário e ainda não devolvido'
+  }
+
+}
