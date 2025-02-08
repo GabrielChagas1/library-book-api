4042d84866e2181bbe1b7808ceced134e4661ba5
Gabriel Chagas <gabrielserq@outlook.com>
2025-02-08 17:14:03 -0300

    :sparkles: feat: add emprestimos.entity.js

---
+++ src/enterprise/entities/emprestimo.entity.js
@@ -0,0 +1,20 @@
+const { AppError } = require("../../shared/errors");
+
+const emprestimosEntity = function () {
+  
+  function calcularDiasAtraso({data_retorno, data_devolucao}){
+    return new Date(data_retorno).getTime() < new Date(data_devolucao).getTime();
+  }
+
+  const calcularMulta = function ({ data_retorno, data_devolucao}){
+
+    if(!data_retorno || !data_retorno) throw new AppError(AppError.parametrosObrigatoriosAusentes)
+
+    const diasAtraso = calcularDiasAtraso({data_retorno, data_devolucao})
+    return `Multa por atraso: R$${diasAtraso > 0 ? '10': '0'}` 
+  }
+
+  return {calcularMulta}
+}
+
+module.exports = emprestimosEntity();
