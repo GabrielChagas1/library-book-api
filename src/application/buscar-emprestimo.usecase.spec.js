992e338589ef0824ef4f2b43f67aebb5aece4ffb
Gabriel Chagas <gabrielserq@outlook.com>
2025-02-21 15:59:35 -0300

    :test_tube: test: add buscar-emprestimo.usecase.spec.js

--- src/application/buscar-emprestimo.usecase.spec.js
+++ src/application/buscar-emprestimo.usecase.spec.js
@@ -1,4 +1,5 @@
 const { AppError } = require("../shared/errors")
+const buscarPendentes = require("../tests/fixtures/buscar-pendentes")
 const buscarEmprestimosUsecase = require("./buscar-emprestimos.usecase")
 
 describe('Buscar emprestimos pendentes UseCase', () => {
@@ -8,30 +9,7 @@ describe('Buscar emprestimos pendentes UseCase', () => {
 
   test('Deve ser possível buscar os empréstimos pendentes', async () => {
 
-    emprestimosRepository.buscarPendentesComLivroComUsuario.mockResolvedValue([
-      {
-        usuario: {
-          nome: 'qualquer_nome_usuario',
-          CPF: 'qualquer_cpf'
-        },
-        livro: {
-          nome: 'qualquer_nome_livro'
-        },
-        data_saida: '2024-10-01',
-        data_retorno: '2024-10-02'
-      },
-      {
-        usuario: {
-          nome: 'qualquer_nome_valido',
-          CPF: 'qualquer_cpf_valido'
-        },
-        livro: {
-          nome: 'qualquer_nome_livro_valido'
-        },
-        data_saida: '2024-10-10',
-        data_retorno: '2024-10-15'
-      }
-    ])
+    emprestimosRepository.buscarPendentesComLivroComUsuario.mockResolvedValue(buscarPendentes)
 
     const sut = buscarEmprestimosUsecase({ emprestimosRepository})
     const output = await sut();
