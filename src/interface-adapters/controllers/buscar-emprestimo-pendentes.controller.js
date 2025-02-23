cfbbceb93078e0f5aaa1cd373e0f2cff9d75ea94
Gabriel Chagas <gabrielserq@outlook.com>
2025-02-22 23:46:52 -0300

    :sparkles: chore: add buscar-emprestimo-pendentes.controller.js

---
+++ src/interface-adapters/controllers/buscar-emprestimo-pendentes.controller.js
@@ -0,0 +1,12 @@
+const httpResponse = require("../../helpers/http.response")
+const { AppError } = require("../../shared/errors")
+
+module.exports = async function buscarEmprestimosPendentesController({buscarEmprestimosPendentesUseCase}){
+  if(!buscarEmprestimosPendentesUseCase) throw new AppError(AppError.dependecias)
+  const output = await buscarEmprestimosPendentesUseCase()
+
+  return output.fold(
+    (error) => httpResponse(400, error.message),
+    (emprestimos) => httpResponse(200, emprestimos)
+  )
+}
