5bc15563681555437c591b59e6aff5b917d87cf6
Gabriel Chagas <gabrielserq@outlook.com>
2025-02-22 23:45:10 -0300

    :sparkles: chore: add setup.js

---
+++ src/infra/db/typeorm/setup.js
@@ -0,0 +1,15 @@
+const typeorm = require('typeorm')
+
+const typeormServer = new typeorm.DataSource({
+  type: 'sqlite',
+  database: 'db.sqlite',
+  synchronize: true,
+  dropSchema: true,
+  entities: [
+    require('./entities/Usuario.entity-typeorm'), 
+    require('./entities/Livros.entity-typeorm'),
+    require('./entities/Emprestimo.entity-typeorm'),
+  ]
+})
+
+module.exports = { typeormServer}
