f9940e234a5e85f355975a956ac2cb59fb7e6191
Gabriel Chagas <gabrielserq@outlook.com>
2025-03-03 23:18:01 -0300

    :sparkles: chore: add cadastrar-usuario.usecase.js

--- src/application/cadastrar-usuario.usecase.js
+++ src/application/cadastrar-usuario.usecase.js
@@ -3,8 +3,8 @@ const {Either} = require('../shared/errors')
 
 module.exports = function cadastrarUsuarioUseCase({ usuariosRepository }){
   if(!usuariosRepository) throw new AppError(AppError.dependecias)
-    return async function({nomeCompleto, CPF, telefone, endereco, email}){
-    const checaCampos = nomeCompleto && CPF && telefone && endereco && email;
+    return async function({nome_completo, CPF, telefone, endereco, email}){
+    const checaCampos = nome_completo && CPF && telefone && endereco && email;
     if(!checaCampos) throw new AppError(AppError.parametrosObrigatoriosAusentes)
     
     const checaSeJaExisteUmUsuarioCadastradoComOCPF = await usuariosRepository.existePorCPF(CPF);
@@ -19,7 +19,7 @@ module.exports = function cadastrarUsuarioUseCase({ usuariosRepository }){
     }
     
     await usuariosRepository.cadastrar({
-      nomeCompleto, CPF, telefone, endereco, email
+      nome_completo, CPF, telefone, endereco, email
     });
 
     return Either.Right(null)
