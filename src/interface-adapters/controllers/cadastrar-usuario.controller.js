66dba1ab03b7dd3d421433436e08ac58708bac27
Gabriel Chagas <gabrielserq@outlook.com>
2025-03-03 23:18:22 -0300

    :sparkles: chore: add cadastrar-usuario.controller.js

--- src/interface-adapters/controllers/cadastrar-usuario.controller.js
+++ src/interface-adapters/controllers/cadastrar-usuario.controller.js
@@ -1,41 +1,48 @@
-const httpResponse = require("../../helpers/http.response");
-const { AppError } = require("../../shared/errors");
-const {z} = require('zod')
+const { z } = require('zod');
+const { AppError } = require('../../shared/errors');
+const httpResponse = require('../../helpers/http.response');
 
 const zodValidator = z.object({
   nome_completo: z.string({
-    required_error: 'Nome completo é obrigatório'
+    required_error: 'Nome Completo é obrigatório'
   }),
-  CPF: z.string({
-    required_error: 'CPF é obrigatório'
-  }).refine((value) => /^([0-9]{3}\.?[0-9]{3}\.[0-9]{3}\-?[0-9]{2})$/.test(value)),
+  CPF: z
+    .string({
+      required_error: 'CPF é obrigatório'
+    })
+    .refine((value) => /^([0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2})$/.test(value)),
   endereco: z.string({
     required_error: 'Endereço é obrigatório'
   }),
   telefone: z.string({
     required_error: 'Telefone é obrigatório'
   }),
-  email: z.string({
-    required_error: 'Endereço é obrigatório'
-  }).email({ message: 'Email deve ser válido '}),
-})
+  email: z
+    .string({
+      required_error: 'Email é obrigatório'
+    })
+    .email({
+      message: 'Email deve ser válido'
+    })
+});
 
-module.exports = async function cadastrarUsuarioController({cadastrarUsuarioUseCase, httpRequest}){
-
-  const checaDependecias = !cadastrarUsuarioUseCase || !httpRequest || !httpRequest.body;
-
-  if(checaDependecias) throw new AppError(AppError.dependecias)
-
-  const {nome_completo, CPF, endereco, telefone, email} = zodValidator.parse(httpRequest.body);
+module.exports = async function cadastrarUsuarioController({
+  cadastrarUsuarioUseCase,
+  httpRequest
+}) {
+  const checaDependencias = !cadastrarUsuarioUseCase || !httpRequest || !httpRequest.body;
+  if (checaDependencias) throw new AppError(AppError.dependencias);
+  const { nome_completo, CPF, endereco, telefone, email } = zodValidator.parse(httpRequest.body);
   const output = await cadastrarUsuarioUseCase({
     nome_completo,
     CPF,
     endereco,
     telefone,
     email
-  })
+  });
+
   return output.fold(
     (error) => httpResponse(400, error.message),
     () => httpResponse(201, null)
-  )
-}
+  );
+};
