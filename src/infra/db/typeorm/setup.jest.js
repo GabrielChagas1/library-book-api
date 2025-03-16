f4aa326ff12fc0443b4d4d6f0ac1920e95f46627
Gabriel Chagas <gabrielserq@outlook.com>
2025-03-15 21:04:42 -0300

    chore: add setup.jest.js

--- src/infra/db/typeorm/setup.jest.js
+++ src/infra/db/typeorm/setup.jest.js
@@ -1,9 +1,24 @@
 const { typeormServer } = require("./setup");
 
 beforeAll(async () => {
-  await typeormServer.initialize()
-})
+  if (!typeormServer.isInitialized) {
+    try {
+      await typeormServer.initialize();
+      console.log("✅ Banco de dados conectado!");
+    } catch (error) {
+      console.error("❌ Erro ao conectar ao banco:", error);
+      process.exit(1);
+    }
+  }
+});
 
 afterAll(async () => {
-  await typeormServer.destroy()
-})
+  if (typeormServer.isInitialized) {
+    try {
+      await typeormServer.destroy();
+      console.log("🔌 Conexão com banco de dados fechada.");
+    } catch (error) {
+      console.error("❌ Erro ao fechar a conexão:", error);
+    }
+  }
+});
